3b742cf35171abaed06b7ec8ef370e6c4a45473b
Uday-sidagana <uday.sidgana@gmail.com>
2025-03-15 23:29:54 +0530

    Instead of regualr boilerplate, using Krish's python boilerplate

--- crew.py
+++ crew.py
@@ -1,46 +1,16 @@
-# src/latest_ai_development/crew.py
-from crewai import Agent, Crew, Process, Task
-from crewai.project import CrewBase, agent, crew, task
-from crewai_tools import SerperDevTool
+from crewai import Crew,Process
+from tasks import research_task,write_task
+from agents import news_researcher,news_writer
 
-@CrewBase
-class LatestAiDevelopmentCrew():
-  """LatestAiDevelopment crew"""
+## Forming the tech focused crew with some enhanced configuration
+crew=Crew(
+    agents=[news_researcher,news_writer],
+    tasks=[research_task,write_task],
+    process=Process.sequential,
 
-  @agent
-  def researcher(self) -> Agent:
-    return Agent(
-      config=self.agents_config['researcher'],
-      verbose=True,
-      tools=[SerperDevTool()]
-    )
+)
 
-  @agent
-  def reporting_analyst(self) -> Agent:
-    return Agent(
-      config=self.agents_config['reporting_analyst'],
-      verbose=True
-    )
+## starting the task execution process wiht enhanced feedback
 
-  @task
-  def research_task(self) -> Task:
-    return Task(
-      config=self.tasks_config['research_task'],
-    )
-
-  @task
-  def reporting_task(self) -> Task:
-    return Task(
-      config=self.tasks_config['reporting_task'],
-      output_file='output/report.md' # This is the file that will be contain the final report.
-    )
-
-  @crew
-  def crew(self) -> Crew:
-    """Creates the LatestAiDevelopment crew"""
-    return Crew(
-      agents=self.agents, # Automatically created by the @agent decorator
-      tasks=self.tasks, # Automatically created by the @task decorator
-      process=Process.sequential,
-      verbose=True,
-    )
+result=crew.kickoff(inputs={'topic':'AI in healthcare'})
+print(result)
