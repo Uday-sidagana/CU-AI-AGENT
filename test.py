cdad1a4e653a407b81937448ec48f560e3c79e68
Uday-sidagana <uday.sidgana@gmail.com>
2025-03-16 15:49:55 +0530

    creating a new agent to look through the student details and fill form

--- test.py
+++ test.py
@@ -7,8 +7,6 @@ import litellm
 load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API")
 
-if not GEMINI_API_KEY:
-    raise ValueError("❌ Missing GEMINI_API in .env file")
 
 # Set environment variable for LiteLLM
 os.environ["GEMINI_API_KEY"] = GEMINI_API_KEY
@@ -16,12 +14,23 @@ os.environ["GEMINI_API_KEY"] = GEMINI_API_KEY
 # Define the LLM with the correct provider
 llm = LLM(model="gemini/gemini-1.5-flash")  # provider="gemini" is optional
 
+#student details
+details ={'name':'Uday', 'number': '9550578004', 'Xth_Marks': 95}
+
 # Define an AI agent
 researcher = Agent(
     role="AI Researcher",
     goal="Summarize AI advancements",
     backstory="Expert in AI and ML research",
     llm=llm
+)
+
+form_filler = Agent(
+    role="Google Form Filler",
+    goal="To fill the google form with student data",
+    backstory="Expert in using Selenium and can fill Google forms",
+    llm= llm
+
 )
 
 # Define the Task
